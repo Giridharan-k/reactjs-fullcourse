6a756abe44cdf9609455e2ddd5487c9673f62021
Giri dharan <giri15kgd@gmail.com>
2025-06-18 12:08:09 +0530

    class-1 variable and fuction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const UserName = "Aditya Prabhu";
+  const Password = "094576768";
+
+  function onSubmit () {
+    alert("on submit clicked")
+  }
+
+  
   return (
-    <div className="App">
-      first class for Aditiya
-    </div>
+    <React.Fragment>
+      <div style={{marginRight:'10%'}}>
+        <b>UserName</b> : {UserName}
+      </div>
+      <div>
+        <b>Password</b> : {Password}
+      </div>
+      <div>
+        <input type='button' value="Submit" onClick={onSubmit} /> 
+      </div>
+    </React.Fragment>
   );
 }
 
